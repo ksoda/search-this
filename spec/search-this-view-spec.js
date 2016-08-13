1aebc2838490b9afa44121d1655d092f257bbe46
Ken Sonoda <ken.soda0@gmail.com>
2016-08-14 00:26:39 +0900

    Initial commit

---
+++ spec/search-this-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SearchThisView from '../lib/search-this-view';
+
+describe('SearchThisView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
